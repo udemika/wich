1924de1f5f4f99a04ebb1e0e49b6fd178d4fc1d3
udemika <aksarus@gmail.com>
2026-09-25 20:56:40 +0500

    Update hdpoisk.js

--- hdpoisk.js
+++ hdpoisk.js
@@ -60,7 +60,7 @@
     function getHost() {
         if (connection_source === 'ab2024') return 'https://akter-black.com/';
         if (connection_source === 'showy') return MIRRORS_SHOWY[current_showy_index];
-        if (connection_source === 'okeantv') return 'http://148.135.207.174:12359/';
+        if (connection_source === 'okeantv') return 'http://108.165.164.64:9118/';
         if (connection_source === 'hdpoisk') return 'https://hdpoisk.ru/';
         if (connection_source === 'lampaua') return 'https://cors.lampa.stream/http://lampaua.mooo.com/';
         if (connection_source === 'beta') return 'http://beta.l-vid.online:888/';
